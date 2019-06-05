86672de9c4ec48d34005b33af8ea335cb94e9834
xMambo <millerdesigncotn@gmail.com>
2019-06-05 12:16:03 -0500

    got the cheerio selectors i need to console log

--- server.js
+++ server.js
@@ -1,7 +1,33 @@
-import { Mongoose } from "mongoose";
+var cheerio = require("cheerio");
+var axios = require("axios");
 
-//If deployed use the deployed deatabase. Otherwise use the local NewsScraper database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb: //localhost/NewScraper";
+// Let's tell the console what the server.js is doing.
+console.log("\n-----------------------------------------------------------------\n" +
+            "Searching for articles from this cool Science web site I found" +
+            "\n-----------------------------------------------------------------\n");
 
-// Connect to the Mongo DB
-Mongoose.connect(MONGODB_URI);
+// Making a request via axios
+axios.get("https://allthatsinteresting.com/tag/news").then(function(response) {
+
+// Load the HTML into cheerio and save it to a variable
+var $ = cheerio.load(response.data);
+
+
+var results = [];
+
+$(".box").each(function(i, element) {
+
+    var title = $(element).find("h2").text();
+    var summary = $(element).find(".summary").text();
+
+    var link = $(element).find("a").attr("href");
+
+    results.push({
+        title: title,
+        summary: summary,
+        link: link
+    });
+});
+
+console.log(results);
+});
